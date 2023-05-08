cda3552d6fea51800a9c89884b78826c24b4a228
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-05-08 13:26:44 +0600

    update data

--- index.js
+++ index.js
@@ -51,7 +51,28 @@ async function run() {
       const result = await coffeeCollection.deleteOne(query);
       res.send(result);
     });
-    g;
+
+    app.put("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedCoffee = req.body;
+
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name,
+          quantity: updatedCoffee.quantity,
+          supplier: updatedCoffee.supplier,
+          taste: updatedCoffee.taste,
+          category: updatedCoffee.category,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo,
+        },
+      };
+
+      const result = await coffeeCollection.updateOne(filter, coffee, options);
+      res.send(result);
+    });
 
     app.get("/coffee/:id", async (req, res) => {
       const id = req.params.id;
